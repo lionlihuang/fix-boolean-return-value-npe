35c162f0c44703087457a2f0347e016e0f1d60c6
dqprecisian <1352034864@qq.com>
2019-10-13 16:52:08 +0800

    解决boolean值类型为null的bug.

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,10 +13,10 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        return on == null? false : on;
     }
 
     public Boolean isOnRawValue() {
-        return on;
+        return on == null? false : on;
     }
 }
