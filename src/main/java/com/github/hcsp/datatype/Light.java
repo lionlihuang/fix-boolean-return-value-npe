f364b2162b97854984ac01862f6635e3aed83fbb
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2019-10-18 16:05:03 +0800

    fix null pointer issue (#97)
    fix null pointer issue (#97)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,13 +8,17 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
-        this.on = on;
+    	this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public boolean isOn() {
-        return on;
-    }
+    public Boolean isOn() {
+    	if(on!=null) {
+    		return on;
+    	}return false;
+    	}
+        
+    
 
     public Boolean isOnRawValue() {
         return on;
