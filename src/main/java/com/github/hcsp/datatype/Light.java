4f30efd42140f5fe5397f028bc61d34a55b0bef2
Storm4542 <417002107@qq.com>
2019-11-25 15:33:14 +0800

    fix bug

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -12,8 +12,9 @@ public class Light {
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public boolean isOn() {
-        return on;
+    public Boolean isOn() {
+        if (on == null) return false;
+        return true;
     }
 
     public Boolean isOnRawValue() {
