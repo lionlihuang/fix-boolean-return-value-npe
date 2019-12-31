bb2fa7f1163fd55c5e4cd3654cb7873ad72c28d0
zaker-666 <1075151259china@gmail.com>
2019-12-31 20:18:25 +0800

    修复了空指针异常

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -7,12 +7,16 @@ public class Light {
     // 未知 -> null
     Boolean on;
 
+
     public Light(Boolean on) {
         this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
+        if(this.on==null){
+            return false;
+        }
         return on;
     }
 
