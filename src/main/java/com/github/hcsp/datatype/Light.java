071125341732c2bbb7c0e13a06c5c78f5b49f4b6
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-01-13 23:11:18 +0800

    Mystery (#150)
    Mystery (#150)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,12 +8,13 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
-        this.on = on;
+            this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        return this.on == null ? false : this.on;
+
     }
 
     public Boolean isOnRawValue() {
