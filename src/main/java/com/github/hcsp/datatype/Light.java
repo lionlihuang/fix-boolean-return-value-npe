78e722880b8c54933ffaaaf9d7bbbd636df4af79
天天天天天 <1500846601@qq.com>
2020-01-22 16:07:12 +0800

    修复空指针异常 (#151)
    修复空指针异常 (#151)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,12 +8,12 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
-        this.on = on;
+            this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        return (on == null) ? false : on;
     }
 
     public Boolean isOnRawValue() {
