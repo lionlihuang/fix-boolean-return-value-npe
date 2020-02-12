a297f2a6744d72d07b12609123f7987aaa7aae37
daimon0809 <51164058+daimon0809@users.noreply.github.com>
2020-02-12 23:49:29 +0800

    修复返回值中出现的空指针异常 (#161)
    修复返回值中出现的空指针异常 (#161)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,7 +13,12 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (on != null){
+            return on;
+        } else {
+            return false;
+        }
+
     }
 
     public Boolean isOnRawValue() {
