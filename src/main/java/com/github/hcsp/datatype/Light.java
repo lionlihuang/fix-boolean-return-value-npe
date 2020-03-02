71625902f938c8e7cba51b033b3e0c37c6639ba7
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-02 14:28:42 +0800

    isNull (#172)
    isNull (#172)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.datatype;
 
+import java.util.Objects;
+
 public class Light {
     // 一盏灯可能有三种状态：
     // 亮 -> true
@@ -13,7 +15,9 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (Objects.isNull(on)) {
+            return false;
+        } else return on;
     }
 
     public Boolean isOnRawValue() {
