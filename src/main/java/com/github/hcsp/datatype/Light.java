e0f8ee62b72a499e054019004ceeead378f73fb3
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-06 00:58:08 +0800

    修复返回值中出现的空指针异常 (#175)
    修复返回值中出现的空指针异常 (#175)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,7 +13,11 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if(on != null) {
+            return on;
+        } else {
+            return false;
+        }
     }
 
     public Boolean isOnRawValue() {
