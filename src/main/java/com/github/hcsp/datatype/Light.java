aef8f1f5c3ca895f8e68959efb300546b588a4c3
liulian1999 <61763395+liulian1999@users.noreply.github.com>
2020-03-09 20:00:01 -0700

    fix (#179)
    fix (#179)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,7 +13,12 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (null == on){
+            return false;
+
+        }
+        else{
+            return on;}
     }
 
     public Boolean isOnRawValue() {
